95040f87780e47d0e30b376fde11a9e694fb9da4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-03 17:13:41 +1100

    Add update and delete functions to Movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+    before_action :set_movie, only: [:show, :edit, :update, :destroy]
     
     # Go to list of Movies
     def index
@@ -7,6 +8,38 @@ class MoviesController < ApplicationController
     
     # Go to a specific Movie
     def show
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new( movie_params )
+        @movie.save
+
+        redirect_to movies_url
+    end
+
+    def edit
+    end
+
+    def update
+        @movie.update( movie_params )
+        redirect_to movie_path
+    end
+
+    def destroy
+        @movie.destroy
+        redirect_to movies_url
+    end
+
+    private
+    def set_movie
         @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+        params.require(:movie).permit(:title, :description, :year_released, :genre)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,20 @@ Rails.application.routes.draw do
   # Show
   get '/movies/:id' => 'movies#show', as: 'movie'
 
+  # New
+  get '/movie/new' => 'movies#new'
+
+  # Create
+  post '/movies' => 'movies#create'
+
+  # Edit
+  get '/movie/:id/edit' => 'movies#edit', as: 'movie_edit'
+
+  # Update
+  patch '/movies/:id' => 'movies#update'
+  put '/movies/:id' => 'movies#update'
+
+  # Delete
+  delete '/movies/:id' => 'movies#destroy', as: 'movie_delete'
+
 end
