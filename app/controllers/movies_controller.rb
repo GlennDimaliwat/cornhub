16d33395e1f9905549e9686fd35dd0d20b2649e9
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-03 21:34:56 +1100

    Update comments and add more scss

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,10 +10,12 @@ class MoviesController < ApplicationController
     def show
     end
 
+    # Go to New movie page
     def new
         @movie = Movie.new
     end
 
+    # Create Movie based on details entered in New Page
     def create
         @movie = Movie.new( movie_params )
         @movie.save
@@ -21,24 +23,29 @@ class MoviesController < ApplicationController
         redirect_to movies_url
     end
 
+    # Go to Edit movie page
     def edit
     end
 
+    # Update Movie based on details entered in Edit Page
     def update
         @movie.update( movie_params )
         redirect_to movie_path
     end
 
+    # Delete the movie
     def destroy
         @movie.destroy
         redirect_to movies_url
     end
 
     private
+    # Set the Movie ID
     def set_movie
         @movie = Movie.find(params[:id])
     end
 
+    # Retrieve the movie model and allow submission of the attributes Title, Description, Year Released and Genre 
     def movie_params
         params.require(:movie).permit(:title, :description, :year_released, :genre)
     end
